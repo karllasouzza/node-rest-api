b54f8dbadcd0a81a03dfea238e5a2aa5c6a4b954
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-17 09:23:44 -0300

    fix(server): add host to app.listen for binding - Specify host as "0.0.0.0" in app.listen - Ensure server is accessible from external networks

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 import { env } from "./env/index.js";
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
   console.log(`Server is running on port ${env.PORT}!`);
 });
