ca0bb257b1c26c172b727a0b55656e27ff845c61
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-17 09:20:11 -0300

    fix(env): update production config path to .env.local
    - Changed the configuration path for production environment from .env to .env.local

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "test") {
   config({ path: ".env.test" });
   console.log(process.env.DATABASE_URL);
 } else if (process.env.NODE_ENV === "production") {
-  config({ path: ".env" });
+  config({ path: ".env.local" });
 } else {
   config();
 }
